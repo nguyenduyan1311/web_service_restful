b0a79d921ecfbabfabb26eb831e008843de82c47
nguyenduyan1311 <annguyenazn1311@gmail.com>
2021-02-19 15:09:09 +0700

    Web Service & RESTful

---
+++ Laravel-RESTful-api/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/customers', [\App\Http\Controllers\CustomerController::class,'index'])->name('customers.all');
+Route::get('/customers/{customerId}', [\App\Http\Controllers\CustomerController::class,'show'])->name('customers.show');
+Route::post('/customers', [\App\Http\Controllers\CustomerController::class,'store'])->name('customers.store');
+Route::put('/customers/{customerId}', [\App\Http\Controllers\CustomerController::class,'update'])->name('customers.update');
+Route::delete('/customers/{customerId}', [\App\Http\Controllers\CustomerController::class,'destroy'])->name('customers.destroy');
---
+++ laravel-blog-restful-api/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/posts', [\App\Http\Controllers\PostController::class,'index'])->name('posts.list');
+Route::get('/posts/create', [\App\Http\Controllers\PostController::class,'create'])->name('posts.create');
+Route::post('/posts/create', [\App\Http\Controllers\PostController::class,'store'])->name('posts.store');
+
